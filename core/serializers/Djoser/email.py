c3d26af3a89f49f422ec3f16fd47defe3fed806c
itz-amethyst <pransermi@gmail.com>
2024-02-06 18:59:38 +0330

    feat & fix: verification token email process

--- core/serializers/Djoser/email.py
+++ core/serializers/Djoser/email.py
@@ -1,67 +1,22 @@
-from django.contrib.auth.tokens import default_token_generator
-from djoser import email , utils
+from djoser import email
 from djoser.conf import settings
+from core.serializers.helper.email_context import fill_context_data
+from core.models.verification_code import VerificationCode
 
-from core.models import VerificationCode
 
 
 class ActivationEmail(email.ActivationEmail):
     def get_context_data(self):
-        # ActivationEmail can be deleted
         context = super().get_context_data()
-        user = context.get("user")
-
-        context["uid"] = utils.encode_uid(user.pk)
-        context["token"] = default_token_generator.make_token(user)
-        context["url"] = settings.ACTIVATION_URL.format(**context)
-
-        verification_code , created = VerificationCode.objects.get_or_create(user = user)
-        verification_code.token = context['token']
-        verification_code.save()
-        return context
-
-
-class ConfirmationEmail(email.ConfirmationEmail):
-    def get_context_data( self ):
-        # ActivationEmail can be deleted
-        context = super().get_context_data()
-        user = context.get("user")
-
-        context["uid"] = utils.encode_uid(user.pk)
-        context["token"] = default_token_generator.make_token(user)
-        context["url"] = settings.ACTIVATION_URL.format(**context)
-
-        verification_code , created = VerificationCode.objects.get_or_create(user = user)
-        verification_code.token = self.context['token']
-        verification_code.save()
-        return context
+        return fill_context_data(context = context, url = settings.ACTIVATION_URL.format(**context), section = VerificationCode.ACTIVATION)
 
 class PasswordResetEmail(email.PasswordResetEmail):
     def get_context_data( self ):
-        # ActivationEmail can be deleted
         context = super().get_context_data()
-        user = context.get("user")
+        return fill_context_data(context = context, url = settings.PASSWORD_RESET_CONFIRM_URL.format(**context), section = VerificationCode.RESET_PASSWORD)
 
-        context["uid"] = utils.encode_uid(user.pk)
-        context["token"] = default_token_generator.make_token(user)
-        context["url"] = settings.ACTIVATION_URL.format(**context)
 
-        verification_code , created = VerificationCode.objects.get_or_create(user = user)
-        verification_code.token = self.context['token']
-        verification_code.save()
-        return context
-
-class PasswordChangedConfirmationEmail(email.PasswordChangedConfirmationEmail):
-    def get_context_data( self ):
-        # ActivationEmail can be deleted
+class UsernameResetEmail(email.UsernameResetEmail):
+    def get_context_data(self):
         context = super().get_context_data()
-        user = context.get("user")
-
-        context["uid"] = utils.encode_uid(user.pk)
-        context["token"] = default_token_generator.make_token(user)
-        context["url"] = settings.ACTIVATION_URL.format(**context)
-
-        verification_code , created = VerificationCode.objects.get_or_create(user = user)
-        verification_code.token = self.context['token']
-        verification_code.save()
-        return context
+        return fill_context_data(context = context, url = settings.USERNAME_RESET_CONFIRM_URL.format(**context), section = VerificationCode.RESET_USERNAME)
