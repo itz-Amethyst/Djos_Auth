22e42ce79d6a3b7de088f60962f1ced8caef5472
itz-amethyst <pransermi@gmail.com>
2024-02-06 18:58:50 +0330

    feat: added new features to verify model

--- core/models/verification_code.py
+++ core/models/verification_code.py
@@ -5,9 +5,22 @@ from core.models import User
 
 
 class VerificationCode(models.Model):
-    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    RESET_PASSWORD = 'reset_password'
+    ACTIVATION = 'activation'
+    RESET_USERNAME = 'reset_username'
+
+    SECTION_CHOICES = [
+        (RESET_PASSWORD , _('Reset Password')) ,
+        (ACTIVATION , _('Activation')) ,
+        (RESET_USERNAME , _('Reset Username')) ,
+    ]
+
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
     token = models.CharField(max_length = 100, unique = True)
     created_date = models.DateTimeField(_("Date Created") , auto_now_add = True)
+    is_active = models.BooleanField(_("Is Active"), default = True)
+    section = models.CharField(_("Section") , max_length = 20 , choices = SECTION_CHOICES, default = 'Not Defined')
+
 
     def __str__(self):
         return f"{self.user.username}-token"
